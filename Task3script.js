9d068eca12647a72d1ab9f7cf53d6fa46a498b92
benedictvimal2 <benedictvimal2@gmail.com>
2024-05-27 11:52:15 +0530

    task 3 created

--- Task3script.js
+++ Task3script.js
@@ -1,11 +1,12 @@
 function reqListener() {
-    console.log(this.responseText);
+    //console.log(this.responseText);
       const countryArr=JSON.parse(this.responseText);
       for (i=0; i<=countryArr.length-1; i++){
     //console.log(countryArr[i]);
       console.log(countryArr[i].name.common);
       console.log(countryArr[i].region);
-      console.log(countryArr[i].region);  
+      console.log(countryArr[i].subregion);
+      console.log(countryArr[i].population);  
       }
       
     }
